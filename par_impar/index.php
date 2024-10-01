ca32d63ab9b679702da46dc587aef353ed171bc8
Johngarsjohn <joaogarciajoao96@gmail.com>
2024-10-01 09:34:07 +0100

    1º commit - adicionar conteúdo html na página

---
+++ par_impar/index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta name="description" content="algoritmo que permite verificar se um número fornecido é par ou ímpar">
+	<meta name="keywords" content="número par, número ímpar">
+	<meta name="author" content="João Garcia João">
+	<title>Número par ou ímpar</title>
+</head>
+<body>
+
+	<div id="container">
+		<main class="corpo">
+			<form method="post" action="">
+				<div>
+					<label for="numero">Número:</label>
+					<input type="number" name="numero" id="numero" required>
+				</div>
+				<div>
+					<input type="submit" name="enviar" value="Verificar">
+				</div>
+				<span>Resultado:</span>
+				
+			</form>
+		</main>
+	</div>
+</body>
+</html>
